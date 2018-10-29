517d1e1755772e7aca7b9d5ac24882e4cfe35dfc
Mattias Johansson <mattias@verodella.se>
2018-10-29 08:20:54 +0100

    Updated templates to ES6

--- app/js/UIX.templates.js
+++ app/js/UIX.templates.js
@@ -1,50 +1,46 @@
 UIX.items = {
     text: {
-        templ:  '<section>' +
-                '   <label>{{label}}</label>' +
-                '   <input id="{{id}}" class="full" type="text" value="{{value}}">' +
-                '</section>',
+        templ: `<section>
+                   <label>{{label}}</label>
+                   <input id="{{id}}" class="full" type="text" value="{{value}}">
+                </section>`,
         defVal: {
             label: 'My Text input',
             value: '',
         }
     },
-
     checkbox: {
-        templ:  '<section>' +
-                '    <input type="checkbox" {{value}} id="{{id}}" name="{{id}}">' +
-                '    <label for="{{id}}">' +
-                '       <span class="unchecked"><i class="fas fa-square"></i> {{label}}</span>' +
-                '       <span class="checked"><i class="fas fa-check-square"></i> {{label}}</span>' +
-                '    </label>' +
-                '</section>',
+        templ: `<section>
+                    <input type="checkbox" {{value}} id="{{id}}" name="{{id}}">
+                    <label for="{{id}}">
+                       <span class="unchecked"><i class="fas fa-square"></i> {{label}}</span>
+                       <span class="checked"><i class="fas fa-check-square"></i> {{label}}</span>
+                    </label>
+                </section>`,
         defVal: {
             label: 'My checkbox',
             value: 'checked' // 'checked' || ''
         }
     },
-
     swish: {
-        templ: '<section>' +
-            '    <input type="checkbox" {{value}} id="{{id}}" name="{{id}}">' +
-            '    <label for="{{id}}">' +
-            '        <span class="unchecked"><i class="fas fa-square"></i> {{label}}</span>' +
-            '        <span class="checked"><i class="fas fa-check-square"></i> {{label}}</span>' +
-            '    </label>' +
-            '</section>',
+        templ: `<section>
+                    <input type="checkbox" {{value}} id="{{id}}" name="{{id}}">
+                    <label for="{{id}}">
+                        <span class="unchecked"><i class="fas fa-square"></i> {{label}}</span>
+                        <span class="checked"><i class="fas fa-check-square"></i> {{label}}</span>
+                    </label>
+                </section>`,
         defVal: {
             label: 'My swish',
             value: 'checked' // 'checked' || ''
         }
     },
-
-
     range: {
-        templ:  '<section>' +
-                '    <label>{{label}}</label>' +
-                '    <input name="{{id}}" id="{{id}}" class="full" type="range" min="{{min}}" max="{{max}}" value="{{value}}" step="{{step}}" oninput="{{id}}OutputId.value = {{id}}.value">' +
-                '    <output name="{{id}}OutputName" id="{{id}}OutputId">{{value}}</output>' +
-                '</section>',
+        templ: `<section>
+                    <label>{{label}}</label>
+                    <input name="{{id}}" id="{{id}}" class="full" type="range" min="{{min}}" max="{{max}}" value="{{value}}" step="{{step}}" oninput="{{id}}OutputId.value = {{id}}.value">
+                    <output name="{{id}}OutputName" id="{{id}}OutputId">{{value}}</output>
+                </section>`,
         defVal: {
             min: 0,
             max: 500,
@@ -54,10 +50,10 @@ UIX.items = {
         }
     },
     button: {
-        templ:  '<button id="{{id}}" href="javascript:"><i class="fa fa-{{icon}}"></i>{{label}}</button>',
+        templ: `<button id="{{id}}" href="javascript:"><i class="fa fa-{{icon}}"></i>{{label}}</button>`,
         defVal: {
             icon: '',
             label: 'My button'
         }
-    }
+    },
 };
